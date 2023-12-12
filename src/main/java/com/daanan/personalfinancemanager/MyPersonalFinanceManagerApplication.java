ad66049f2d6a55474e247923604891dd4f71373e
Daanan Dasilva <daanandasilva@gmail.com>
2023-12-12 12:49:39 -0700

    Initial commit

---
+++ src/main/java/com/daanan/personalfinancemanager/MyPersonalFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.daanan.personalfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalFinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalFinanceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daanan/personalfinancemanager/MyPersonalFinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daanan.personalfinancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalFinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
